195d7bac8f06449381a0d03ee480eea82514c3fe
Stephany Oliveira <disasxster@aol.com>
2022-04-19 18:54:02 -0300

    inclui os exercício de Strings e Arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,85 @@
+// Exercícios de interpretação de código
+
+/*
+Enunciado 1:
+
+a. undefined
+b. null
+c. 11
+d. 3
+e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+f. 9
+*/
+
+/*
+Enunciado 2:
+SUBI NUM ÔNIBUS EM MIRROCOS, 27 
+*/
+
+// Exercícios de escrita de código
+
+// Enunciado 1:
+
+// let nomeDoUsuario = prompt(`Insira seu nome:`);
+// let emailDoUsuario = prompt(`Insira seu e-mail:`);
+// console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`);
+
+
+// Enunciado 2:
+
+// let comidasFavoritas = ['Pimentão Recheado', 'Bolo de Chocolate', 'Lasanha de Berinjela', 'Frango Assado com Batata', 'Sorvete de Pistache'];
+
+// a.
+// console.log(comidasFavoritas);
+
+// b.
+// let ultimo = comidasFavoritas.length - 1;
+// console.log('Essas são as minhas comidas preferidas:');
+// console.log(comidasFavoritas[0])
+// console.log(comidasFavoritas[1])
+// console.log(comidasFavoritas[2])
+// console.log(comidasFavoritas[3])
+// console.log(comidasFavoritas[4])
+
+// c.
+
+// let suaComidaFavorita = prompt(`Qual é a sua comida favorita?`);
+// comidasFavoritas.splice(1, 1);
+// comidasFavoritas.splice(1, 0, suaComidaFavorita);
+// console.log('Essas são as nossas comidas preferidas:');
+// console.log(comidasFavoritas[0])
+// console.log(comidasFavoritas[1])
+// console.log(comidasFavoritas[2])
+// console.log(comidasFavoritas[3])
+// console.log(comidasFavoritas[4])
+
+// Enunciado 3:
+
+// // a.
+// let listaDeTarefas = [];
+// // b.
+// let tarefa1 = prompt(`Escreva aqui sua primeira tarefa do dia:`);
+// let tarefa2 = prompt(`Escreva aqui sua segunda tarefa do dia:`);
+// let tarefa3 = prompt(`Escreva aqui sua terceira tarefa do dia:`);
+// listaDeTarefas.push(tarefa1, tarefa2, tarefa3);
+// // c.
+// console.log(listaDeTarefas)
+// // d.
+// let tarefaCumprida = prompt(`Qual dessas tarefas você já realizou hoje? 1, 2 ou 3?`);
+// tarefaCumprida = tarefaCumprida - 1;
+// // e.
+// listaDeTarefas.splice(tarefaCumprida, 1);
+// // f.
+// console.log(listaDeTarefas)
+
+// Desafio
+// 1.
+// let frase = prompt(`Escreva uma frase:`);
+// let array = frase.split(' ');
+// console.log(array)
+
+// 2.
+// let array = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+// console.log(array.indexOf("Abacaxi"))
+// console.log(array.length)
+

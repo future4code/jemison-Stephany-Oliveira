74a1d76046ab85938364b19d27613c682d9b246b
Stephany Oliveira <disasxster@aol.com>
2022-07-14 21:14:43 -0300

    primeiro conjunto de dados referente ao projeto LabeCommerce

---
+++ modulo3/thinking-react/mockDeDados.js
@@ -0,0 +1,38 @@
+    const produtos = [
+        {
+            id:1,
+            name: 'Viagem para Mercúrio',
+            price:14.000,
+            img:'https://im.indiatimes.in/content/2022/May/2_feature_1600x900_mercury_628e098e2f41e.jpg'
+        },
+        {
+            id:2,
+            name: 'Viagem para Vênus',
+            price:22.000,
+            img:'https://cdn.mos.cms.futurecdn.net/pR9pCBjB7tcodWJow8XVGU.jpg'
+        },
+        {
+            id:3,
+            name: 'Viagem para Marte',
+            price:34.000,
+            img:'https://www.worldatlas.com/r/w2560-q80/upload/4c/43/3a/shutterstock-1724343382.jpg'
+        },
+        {
+            id:4,
+            name: 'Viagem para Júpiter',
+            price:54.000,
+            img:'https://cdn.mos.cms.futurecdn.net/Cvzdqd7AxLzjzQNoZaQVhC.jpg'
+        },
+        {
+            id:5,
+            name: 'Viagem para Urano',
+            price:70.000,
+            img:'https://cdn.mos.cms.futurecdn.net/b3Nqnpyv2YS6iwrw4EB7pZ.jpg'
+        },
+        {
+            id:6,
+            name: 'Viagem para Netuno',
+            price:97.000,
+            img:'https://media.wired.com/photos/5d04045bde1abfe4e801d054/125:94/w_1595,h_1200,c_limit/Science-Neptune-FA-PIA01492_orig.jpg'
+        }
+    ]

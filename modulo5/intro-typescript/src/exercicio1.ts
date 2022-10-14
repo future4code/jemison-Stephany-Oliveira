58741cb1cf3ef270106168dfb4c06ce283fb8245
sterx17 <disasxster@aol.com>
2022-10-14 20:03:40 -0300

    <build> resolução dos exercícios da aula inicial de typescript

---
+++ modulo5/intro-typescript/src/exercicio1.ts
@@ -0,0 +1,13 @@
+// exercício 1
+
+function checaTriangulo(a: number, b: number, c: number): string {
+    if (a !== b && b !== c) {
+      return "Escaleno";
+    } else if (a === b && b === c) {
+      return "Equilátero";
+    } else {
+      return "Isósceles";
+    }
+}
+
+console.log(checaTriangulo(13, 2, 2))

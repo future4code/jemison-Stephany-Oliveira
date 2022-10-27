4953aaf01ffcce1ae8b7343b9d1172e3a770832c
sterx17 <disasxster@aol.com>
2022-10-27 18:01:17 -0300

    [express] exercícios 1 ao 6 concluídos

---
+++ modulo5/aprofundamento-express/src/todos.ts
@@ -0,0 +1,34 @@
+import { ToDo } from "./types";
+
+export const allToDos:ToDo[] = [
+    {
+        userID:1,
+        id:1,
+        title:'Testing To Dos',
+        completed: true
+    },
+    {
+        userID:2,
+        id:2,
+        title:'Wash Dishes',
+        completed: false
+    },
+    {
+        userID:2,
+        id:3,
+        title:'Walk Dog',
+        completed: true
+    },
+    {
+        userID:3,
+        id:4,
+        title:'Get Mail',
+        completed: false
+    },
+    {
+        userID:3,
+        id:5,
+        title:'Buy Groceries',
+        completed: false
+    },
+]
---
+++ modulo5/aprofundamento-express/src/types.ts
@@ -0,0 +1,7 @@
+
+export type ToDo = {
+    userID:number,
+    id:number,
+    title:string,
+    completed:boolean
+}
